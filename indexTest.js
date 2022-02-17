5d8a1449650f82f24ed4edc157f5b8aad40b09bb
Sebastián <69972223+seba-campo@users.noreply.github.com>
2022-02-17 16:44:54 -0300

    agregados el lector de argumentos

--- indexTest.js
+++ indexTest.js
@@ -3,16 +3,48 @@ const pelis = require("./pelisTest.js");
 function parsearARGV() {
   //... acá ocurre la magia
   const arguments = process.argv.slice(2);
+  
+  let argObj = {
 
-  var argReturn = {
-      noFormat: true,
-      search: "testSearch",
-      sort: "testSort"      
+  };
+
+  // Buscar los diferentes parametros
+
+  if(arguments.includes('--no-format') || arguments.includes('--no-Format') ){
+    // const argumentIndex = arguments.indexOf('--no-format');
+    // const parameterValue = true;
+    argObj.noFormat = true;
+  } else {
+    argObj.noFormat = false;
   }
-  
-  
-  return argReturn
+  ;
+
+  if(arguments.includes('--search')){
+    const argumentIndex = arguments.indexOf('--search');
+    const parameterValue = arguments[argumentIndex + 1];
+
+    argObj.search = parameterValue;
+  };
+
+  if(arguments.includes('--sort')){
+    const argumentIndex = arguments.indexOf('--sort');
+    const parameterValue = arguments[argumentIndex + 1];
+
+    argObj.sort = parameterValue;
+  };
+
+  if(arguments.includes('--tag')){
+    const argumentIndex = arguments.indexOf('--tag');
+    const parameterValue = arguments[argumentIndex + 1];
+
+    argObj.tag = parameterValue;
+  };
+
+  // console.log(arguments);
+
+  return argObj
 }
+
 
 function main() {
   const comandosAEjecutar = parsearARGV();
--- pelisTest.js
+++ pelisTest.js
@@ -1,57 +1,96 @@
 const lista = require("./pelis.json");
 
 const getAll = function () {
-    // retorna todas las pelis (collection)
-    // acá hay que leer el archivo y parsearlo 
-    const table = console.table(lista);
-    return table
-  };
+  // retorna todas las pelis (collection)
+  // acá hay que leer el archivo y parsearlo 
+  const arrayDePelis = lista;
 
-  const noFormat = function(){
-      const parsed = JSON.stringify(lista);
-      return parsed
-  };
+  for (let i = 0; i < arrayDePelis.length; i++) {
+    const element = arrayDePelis[i];
+    
+    element.forEach( x => {
+      if(typeof x == 'string'){
+        x.toLowerCase();
+      }
+    });
+    
+
+  }
   
-  const searchBy = function (texto, arrayDePelis) {
-    // recibe un array de pelis y las filtra por texto
-    // acá hay que usar el método filter de array 
-    // console.log("hola desde SearchBy");
-  };
-  const sortBy = function (propiedad, arrayDePelis) {
-    // recibe un array de pelis y las ordena por propiedad
-    // acá se ordena usando el método sort usando el parámetro "propiedad"
-    // tiene que poder ordenar el array contemplando que me pueden pasar cualquier cosa en 
-    // el parámetro "propiedad", no solo rating y title
-    // Pista: https://flaviocopes.com/how-to-sort-array-of-objects-by-property-javascript/
-    // console.log("hola desde sortBy");
-  };
   
-  exports.searchByCriteria = function (criterios) {
-    // comienzo un array vacio que voy a empezar a rellenar con las respuestas de las funciones
-    let tmp = getAll();
-
-    console.log(criterios);
-
-    if(criterios.noFormat){
-        tmp = console.log(noFormat());
-    }else{
-        // console.log("no hay 'noFormat'");
-    }
+  return arrayDePelis
+  
+};
 
 
-    if (criterios.search){ 
-      // console.log("hay search y es", criterios.search);
-      tmp = searchBy(criterios.search, tmp);
-    } else {
-      console.log("no hay search");
-    }
-
-    if (criterios.sort) {
-      tmp = sortBy(criterios.sort, tmp);
-      // console.log("hay sort y es", criterios.sort);
-    } else {
-      console.log("no hay sort");
-    }
-    // .. y asi
-    return tmp;
+const noFormat = function(){
+    const parsed = JSON.stringify(lista);
+    return parsed
   };
+
+const searchBy = function (texto, arrayDePelis) {
+  // recibe un array de pelis y las filtra por texto
+  // acá hay que usar el método filter de array 
+  // console.log("hola desde SearchBy");
+
+  
+    // var tmp = arrayDePelis.filter(function(x){
+  //   });
+
+    return arrayDePelis
+  
+};
+
+
+const sortBy = function (propiedad, arrayDePelis) {
+  // recibe un array de pelis y las ordena por propiedad
+  // acá se ordena usando el método sort usando el parámetro "propiedad"
+  // tiene que poder ordenar el array contemplando que me pueden pasar cualquier cosa en 
+  // el parámetro "propiedad", no solo rating y title
+  // Pista: https://flaviocopes.com/how-to-sort-array-of-objects-by-property-javascript/
+  // console.log("hola desde sortBy");
+  };
+
+const sortByTag = function (propiedad, arrayDePelis) {
+
+  };
+
+
+
+exports.searchByCriteria = function (criterios) {
+  // comienzo un array vacio que voy a empezar a rellenar con las respuestas de las funciones
+  let tmp = getAll();
+
+  console.log(tmp);
+  // console.log(criterios);
+
+  if (criterios.noFormat){
+      tmp = console.log(noFormat());
+  }else{
+      // console.log("no hay 'noFormat'");
+  }
+
+
+  if (criterios.search){ 
+    console.log("hay search y es", criterios.search);
+    tmp = searchBy(criterios.search, tmp);
+  } else {
+    console.log("no hay search");
+  }
+
+  if (criterios.sort){
+    tmp = sortBy(criterios.sort, tmp);
+    // console.log("hay sort y es", criterios.sort);
+  } else {
+    console.log("no hay sort");
+  }
+  
+  if (criterios.tag){ 
+    tmp = sortByTag(criterios.tag, tmp);
+  } else {
+    console.log("no hay tags")
+  }
+
+
+  return tmp;
+};
